35dd5fde68bdf4002793351fffa3075887a992d2
Timowest12 <timowester@gmail.com>
2022-01-31 18:24:50 +0100

    use guard class instead of if

--- corrector.rb
+++ corrector.rb
@@ -1,9 +1,6 @@
 class Corrector
   def correct_name(name)
     name = name.capitalize
-    if name.length >= 10
-      name = name[0...10]
-      puts name
-    end
+    name[0...10] if name.length > 10
   end
 end
