10d8b9fe14710c753b5ee9e3eef796668618dd29
Timo <timowester@gmail.com>
2022-02-05 19:19:29 +0100

    Add basic menu

--- main.rb
+++ main.rb
@@ -14,5 +14,29 @@ class App
     @class = Classroom.new('Group 6')
   end
   
+  def run
+    puts 'Welcome!'
+    loop do
+      menu
+      option = gets.chomp
+      break if option == '7'
+
+      get_num option
+    end
+    puts 'Thank you for using our Library!'
+  end
+  
+    def menu
+    puts
+    puts 'Please choose an option by entering a number'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
+  
 end
 main
